89b03b317bc9368c04125d9021d22824895df7e2
xiaoxiaorenwu     email=2379448326@qq.com <2379448326@qq.com>
2019-07-07 17:47:40 +0800

    Site updated: 2019-07-07 17:47:31

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -7,12 +7,6 @@ $(document).ready(function () {
         "/images/4.jpg",
         "/images/5.jpg",
         "/images/6.jpg",
-        "/images/7.jpg",
-        "/images/8.jpg",
-        "/images/9.jpg",
-        "/images/10.jpg",
-        "/images/11.jpg",
-        "/images/12.jpg",
     ];
     var randomIndex = Math.floor(Math.random() * (backgroundImgUrls.length + 1));
     if (randomIndex == backgroundImgUrls.length) {
